4eb3c07fe781f2b2dc29f690ca7fd13bc0a4b2f2
KaidynPhelan <kaidyn.phelan08@gmail.com>
2022-06-28 20:20:08 +1000

    Refactored CSS and changed loading to color white

--- src/components/Article.js
+++ src/components/Article.js
@@ -13,12 +13,12 @@ function Article({ article }) {
 
   return (
       <Card className='Card'>
-        <Card.Header className="article-header">
+        <Card.Header className="article-Header">
           <Card.Title style={{  margin: '0' }}>{cleanDetails(article.title)}</Card.Title>
 
           {/* Create Video Pill if Article contains a Video Link */}
           {article.hasVideo && 
-            <a className = "VideoPill" href={article.videoUrl}>
+            <a className = "videoPill" href={article.videoUrl}>
               Video
             </a>
           }
