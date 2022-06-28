617d39e0352b00b90d329a7f6893ffd58d8d1ed4
KaidynPhelan <kaidyn.phelan08@gmail.com>
2022-06-28 14:48:56 +1000

    Updates

---
+++ precache-manifest.86f5a133ddd0dd411c4fca1471b548d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d49b8f329f0df8a3f063",
+    "url": "/PinkBikeclient/static/css/main.97d0ceb2.chunk.css"
+  },
+  {
+    "revision": "d49b8f329f0df8a3f063",
+    "url": "/PinkBikeclient/static/js/main.d49b8f32.chunk.js"
+  },
+  {
+    "revision": "432553ec76d114c9343e",
+    "url": "/PinkBikeclient/static/css/1.592ea02a.chunk.css"
+  },
+  {
+    "revision": "432553ec76d114c9343e",
+    "url": "/PinkBikeclient/static/js/1.432553ec.chunk.js"
+  },
+  {
+    "revision": "4c747f3eeefeeec5f6e7",
+    "url": "/PinkBikeclient/static/js/runtime~main.4c747f3e.js"
+  },
+  {
+    "revision": "b5abc640cc24b70fbfae3aa09f8e59f1",
+    "url": "/PinkBikeclient/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pinkbikeclient/precache-manifest.7fd8fddd3590c307189e1ce4fd4120e7.js"
+  "/PinkBikeclient/precache-manifest.86f5a133ddd0dd411c4fca1471b548d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pinkbikeclient/index.html", {
+workbox.routing.registerNavigationRoute("/PinkBikeclient/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
